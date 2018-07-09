227ca1641feb1018d97becf6aab707280ed046cc
Andrii Shoferivskyi <andrii_shoferivskyi@epam.com>
2018-07-09 18:29:57 +0300

    Chanhe webpack

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrii_Shoferivskyi on 2018-07-09.
+ */
+
+module.exports = {
+    plugins: {
+        autoprefixer: { browsers: ['last 10 versions'] }
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'src');
+const dist = path.join(__dirname, 'dist');
+const app = path.join(__dirname, 'src/');
 
-const extractCSS = new ExtractTextPlugin(APP_DIR + '/styles/[name].css');
-const extractLESS = new ExtractTextPlugin(APP_DIR + './styles/[name].less');
+const extractCSS = new ExtractTextPlugin(app + '/styles/[name].css');
+const extractLESS = new ExtractTextPlugin(app + './styles/[name].less');
+
+const env = process.argv[process.argv.length - 1];
+const isProduction = env === 'production';
 
 module.exports = {
+  context: __dirname,
   entry: {
-    index: APP_DIR + '/js/index',
-    style: APP_DIR + "/styles/style.less"
+    index: app + '/js/index',
+    style: app + "/styles/style.less"
   },
   output: {
-    path: BUILD_DIR ,
+    path: dist ,
     filename: 'app.js'
   },
 
@@ -23,46 +29,86 @@ module.exports = {
  	rules: [
         {
             test: /\.css$/,
-            use: extractCSS.extract(["css-loader"]),
+            use: extractCSS.extract({
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        minimize: isProduction
+                    }
+                }, 'postcss-loader']
+            })
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', 'es2015', 'es2016', 'es2017']
+                }
+            },  'eslint-loader']
+
         },
         {
             test: /\.less$/,
-            use: extractLESS.extract(["css-loader", "less-loader"]),
-        }/*,
-        {
+            use: extractLESS.extract({
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        minimize: isProduction
+                    }
+                }, "postcss-loader", "less-loader"]
+            })
+        }
+        , {
             test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-            use: "file?name=[path][name].[ext]",
-        }*/
-    ],
-	  loaders : [{
-        test : /\.js$/,
-        exclude: /node_modules/,
-        loader : 'babel-loader',
-        query: {
-          presets: ['es2015', 'es2016', 'es2017']
-      }
-      }]
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 1024
+                }
+            }]
+        }
+
+        // {
+        //     test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+        //     use: "file?name=[path][name].[ext]"
+        // }
+    ]
   },
 
-  plugins: [ 
+  plugins: [
+      new CleanWebpackPlugin(dist),
       extractLESS, 
-      extractCSS, 
-      new webpack.optimize.CommonsChunkPlugin({
-          name: 'common',
-          minChunks: 2
-      })
+      extractCSS
   ],
-  watch: true,
+  watch: !isProduction,
   watchOptions: {
       aggregateTimeout: 100
   },
 
-  devtool: "cheap-inline-module-source-map"
- /* devServer: {
-    inline:true,
-	contentBase: BUILD_DIR
-    port: 8000
-  },
-  */
-  
+    devtool: !isProduction ? "source-map" : false,
+
+    devServer: {
+        contentBase: dist,
+        port: 8000,
+        compress: true
+    },
+
+    optimization: {
+        minimize: isProduction,
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    //test:/[\\/]node_modules[\\/]/,
+                    name: "commons",
+                    chunks: "initial",
+                    minChunks: 2,
+                    maxInitialRequests: 5,
+                    minSize: 0
+                }
+            }
+        }
+    }
+
 };
